8864e4dafd47b976bcd51fdad49667a88e38aad8
lemon <19805815940@163.com>
2019-05-08 22:19:13 +0800

    🙌 add list, registration, login page skeleton.

---
+++ app/view/list/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class List extends Component {
+  render() {
+    return (
+      <View>
+        <Text>List</Text>
+      </View>
+    );
+  }
+}
+
+export default List;
--- app/view/login/index.js
+++ app/view/login/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { common } from '../../styles/index';
 
 class Login extends Component {
   render() {
     return (
-      <View>
-        <Text>Login</Text>
+      <View style={common.layout_flex_middle()}>
+        <Text
+          onPress={() => {
+            this.props.navigation.navigate('Register');
+          }}
+        >
+          Login
+        </Text>
       </View>
     );
   }
