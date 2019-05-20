f4616178c3ea968546dd6b46719a39f8378c1b3a
lemon <19805815940@163.com>
2019-05-20 23:04:24 +0800

    💥 override Settings page.

--- app/view/setting/index.js
+++ app/view/setting/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import Header from '../../components/widget/Header';
 import { common } from '../../styles/index';
 import Button from '../../components/widget/Button';
 import { asyncDelete } from '../../plugin/asyncStorage';
 import constance from '../../plugin/constance';
 import Toast from 'react-native-root-toast';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-class Setting extends Component {
+export default class Setting extends Component {
   loginOut = () => {
     asyncDelete(constance.USER_INFO);
     this.props.navigation.replace('Desk');
@@ -19,10 +20,69 @@ class Setting extends Component {
     return (
       <View style={[common.flex(), common.bgc()]}>
         <Header title='设置' navigation={navigation} />
-        <Button title='退出登录' onPress={this.loginOut} />
+        <View style={{ marginTop: 16 }}>
+          <TouchableOpacity
+            style={styles.article}
+            onPress={() => {
+              Toast.show(`很干净，无需清除`, { position: 0 });
+            }}
+          >
+            <Text style={styles.left}>清除缓存</Text>
+            <View style={styles.right}>
+              <Text style={styles.right_text}>0.00M</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.article}
+            onPress={() => {
+              this.props.navigation.navigate('About');
+            }}
+          >
+            <Text style={styles.left}>关于我们</Text>
+            <View style={styles.right}>
+              <Icon name='right' style={styles.icon} />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <Button
+          title='退出登录'
+          onPress={this.loginOut}
+          style={{
+            touch: {
+              height: 36,
+              marginTop: 20
+            }
+          }}
+        />
       </View>
     );
   }
 }
 
-export default Setting;
+const styles = StyleSheet.create({
+  article: {
+    ...common.screenHeight(52 / 812),
+    ...common.screenWidth(360 / 375),
+    marginLeft: 15,
+    borderBottomColor: '#EEEEEE',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  left: {
+    ...common.fontColorSize('#333333', 16)
+  },
+  right: {
+    marginRight: 15,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  icon: {
+    ...common.fontColorSize('#C4C8CD', 20),
+    marginLeft: 10
+  },
+  right_text: {
+    ...common.fontColorSize('#BEC2C8', 14)
+  }
+});
